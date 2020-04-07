4f13d25de4cf39ebc63e45a835d7a359275b2233
AntonLettuce <antonlettuce@gmail.com>
2020-04-07 16:32:42 +0300

    make step 2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,88 @@
 import axios from 'axios';
 import { promises as fsPromises } from 'fs';
 import path from 'path';
+import cheerio from 'cheerio';
 
-const makeClearName = (url) => {
-  const myUrl = new URL(url);
-  const urlWithoutProtocol = `${myUrl.host}${myUrl.pathname}`;
-  const charsForFilter = ['.', '/'];
-  const filteredChars = urlWithoutProtocol.split('').map((char) => {
-    if (charsForFilter.includes(char)) {
-      return '-';
-    }
-    return char;
-  });
-  const newName = filteredChars.join('');
-  return `${newName}.html`;
+const mapping = {
+  link: 'href',
+  script: 'src',
+  img: 'src',
 };
 
-export default (url, dir) => {
-  const newName = makeClearName(url);
-  const newPath = path.join(dir, newName);
+const makeClearName = (url, base = url) => {
+  const objURL = new URL(url, base);
+  if (url !== base) {
+    const supFile = objURL.pathname;
+    return supFile.split('/').join('-').slice(1);
+  }
+  const urlPath = objURL.pathname.length === 1 ? '' : objURL.pathname;
+  const href = `${objURL.hostname}${urlPath}`;
+  return href.replace(/[^a-zA-Z0-9]+/g, '-');
+};
+
+export default (url, dir = process.cwd()) => {
+  const base = url;
+  const mainFileName = makeClearName(url);
+  const mainFilePath = path.join(dir, mainFileName);
+  let htmlData;
   return axios
     .get(url)
-    .then((parsedData) => parsedData.data)
+    .then((parsedData) => {
+      htmlData = parsedData.data;
+      return htmlData;
+    })
     .then((final) => fsPromises.access(dir)
-      .then(() => fsPromises.writeFile(newPath, final))
+      .then(() => fsPromises.writeFile(`${mainFilePath}.html`, final))
       .catch(() => fsPromises.mkdir(`.${dir}`, { recursive: true })
-        .then(() => fsPromises.writeFile(`.${newPath}`, final))))
-    .catch(console.log);
+        .then(() => fsPromises.writeFile(`.${mainFilePath}.html`, final))))
+    .then(() => {
+      const $ = cheerio.load(htmlData);
+      const tagUrls = Object.keys(mapping).map((tag) => $(tag).map((i, el) => {
+        const supportFileUrl = $(el).attr(mapping[tag]);
+        return supportFileUrl;
+      }).get()).flat().filter((url1) => !url1.includes('//'));
+      return tagUrls;
+    })
+    .then((tagUrls) => {
+      if (tagUrls.length > 0) {
+        let dirPath;
+        if (dir !== process.cwd()) {
+          dirPath = path.join(`.${dir}`, `${mainFileName}_files`);
+        } else {
+          dirPath = `${mainFileName}_files`;
+        }
+        fsPromises.mkdir(dirPath);
+        return tagUrls;
+      }
+      return null;
+    })
+    .then((links) => {
+      const newLinks = links.map((link) => {
+        const supportFileLink = new URL(link, url);
+        const supportFileName = makeClearName(link, base);
+        let supportFilePath = path.join(`${mainFileName}_files`, supportFileName);
+        if (dir !== process.cwd()) {
+          supportFilePath = path.join(`.${dir}`, `${mainFileName}_files`, supportFileName);
+        }
+        axios
+          .get(supportFileLink.href)
+          .then((parsedData) => parsedData.data)
+          .then((data) => {
+            fsPromises.writeFile(supportFilePath, data);
+          });
+        return supportFilePath;
+      });
+      return [links, newLinks];
+    })
+    .then((newUrls) => {
+      const [oldLinks, newLinks] = newUrls;
+      const newHtml = oldLinks
+        .reduce((acc, val, index) => acc.replace(val, newLinks[index]), htmlData);
+      return newHtml;
+    })
+    .then((result) => {
+      const newMainFilePath = dir === process.cwd() ? `${mainFilePath}.html` : `.${mainFilePath}.html`;
+      fsPromises.writeFile(newMainFilePath, result);
+    })
+    .catch(() => console.log('There is no local files to download'));
 };
