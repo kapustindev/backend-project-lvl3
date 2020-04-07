e58b84e2f1e325c08cb8fee73b3f5671edd7c2f9
AntonLettuce <antonlettuce@gmail.com>
2020-04-07 16:35:19 +0300

    make step 2

--- __tests__/simple-download.js
+++ __tests__/simple-download.js
@@ -2,7 +2,7 @@ import nock from 'nock';
 import { promises as fsPromises } from 'fs';
 import path from 'path';
 import os from 'os';
-import pageLoader from '..';
+import pageLoader from '../src';
 
 nock.disableNetConnect();
 
@@ -10,14 +10,23 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 test('make file', async () => {
   const tempDir = os.tmpdir();
-  const fixturePath = getFixturePath('testing-file.html');
-  const result = await fsPromises.readFile(fixturePath, 'utf-8');
-  const data = '<html>\n<head>Testing</head>\n</html>\n';
+  const beforeFixturePath = getFixturePath('testing-file.html');
+  const before = await fsPromises.readFile(beforeFixturePath, 'utf-8');
   nock(/antonlettuce\.github\.io/)
     .get('/hexlet-basics/testing-file')
-    .reply(200, data);
+    .reply(200, before);
+  nock(/antonlettuce\.github\.io/)
+    .get('/assets/application.js')
+    .reply(200, 'simplecode');
+  nock(/antonlettuce\.github\.io/)
+    .get('/actions/duplication.js')
+    .reply(200, 'testcode');
   await pageLoader('https://antonlettuce.github.io/hexlet-basics/testing-file', tempDir);
   const downloadedFilePath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file.html');
+  const supportFilesPath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file_files');
   const newFile = await fsPromises.readFile(downloadedFilePath, 'utf-8');
-  expect(newFile).toEqual(result);
+  const supportFiles = await fsPromises.readdir(supportFilesPath);
+  expect(newFile).toContain('antonlettuce-github-io-hexlet-basics-testing-file_files/actions-duplication.js');
+  expect(newFile).toContain('antonlettuce-github-io-hexlet-basics-testing-file_files/assets-application.js');
+  expect(supportFiles).toEqual(['actions-duplication.js', 'assets-application.js']);
 });
