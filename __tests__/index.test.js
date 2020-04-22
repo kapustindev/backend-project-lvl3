10fda247d1062d1f87be528a4cb396d5e9f6cb8a
AntonLettuce <antonlettuce@gmail.com>
2020-04-22 16:41:27 +0300

    update tests and fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,59 +2,57 @@ import nock from 'nock';
 import { promises as fsPromises } from 'fs';
 import path from 'path';
 import os from 'os';
-import debug from 'debug';
 import pageLoader from '../src';
 import 'axios-debug-log';
 
-const log = debug('nock');
 
 nock.disableNetConnect();
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const responseData = getFixturePath('testing-file.html');
-const resultData = getFixturePath('changedUrls.html');
-const domain = 'https://antonlettuce.github.io';
 
-let tempDir;
-let response;
-
-beforeAll(async () => {
-  tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  response = await fsPromises.readFile(responseData, 'utf-8');
-});
 
 describe('correct data test', () => {
+  const responseData = getFixturePath('before.html');
+  const resultData = getFixturePath('after.html');
+  const site = new URL('https://antonlettuce.github.io/hexlet-basics/testing-file');
+  const domain = site.origin;
+
   test('basic download', async () => {
+    const tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+    const response = await fsPromises.readFile(responseData, 'utf-8');
+    const expectedHtml = await fsPromises.readFile(resultData, 'utf-8');
+    const downloadedFilePath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file.html');
+    const assetPath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file_files', 'assets-application.js');
+
     nock(domain)
-      .get('/hexlet-basics/testing-file')
+      .get(site.pathname)
       .reply(200, response)
-      .log((info) => log(info));
+      .log(console.log);
     nock(domain)
       .get('/assets/application.js')
       .reply(200, 'simplecode')
-      .log((info) => log(info));
+      .log(console.log);
     nock(domain)
       .get('/actions/duplication.js')
       .reply(200, 'testcode')
-      .log((info) => log(info));
-    await pageLoader('https://antonlettuce.github.io/hexlet-basics/testing-file', tempDir);
-    const downloadedFilePath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file.html');
-    const assetsPath = path.join(tempDir, 'antonlettuce-github-io-hexlet-basics-testing-file_files', 'assets-application.js');
-    const result = await fsPromises.readFile(resultData, 'utf-8');
-    const downloadedFile = await fsPromises.readFile(downloadedFilePath, 'utf-8');
-    const assets = await fsPromises.readFile(assetsPath, 'utf-8');
-    expect(downloadedFile).toEqual(result);
-    expect(assets).toContain('simplecode');
-  });
-});
+      .log(console.log);
 
-describe('error tests', () => {
+    await pageLoader('https://antonlettuce.github.io/hexlet-basics/testing-file', tempDir);
+
+    const actualHtml = await fsPromises.readFile(downloadedFilePath, 'utf-8');
+    const asset = await fsPromises.readFile(assetPath, 'utf-8');
+
+    expect(actualHtml).toEqual(expectedHtml);
+    expect(asset).toContain('simplecode');
+  });
+
   test('404 error', async () => {
     nock(domain)
-      .get('/hexlet-basics/404testing-file')
+      .get(site.pathname)
       .reply(404)
-      .log((info) => log(info));
-    await expect(pageLoader('https://antonlettuce.github.io/hexlet-basics/404testing-file')).rejects.toThrow();
+      .log(console.log);
+
+    await expect(pageLoader('https://antonlettuce.github.io/hexlet-basics/testing-file')).rejects.toThrow();
   });
 
   test('no output directory', async () => {
